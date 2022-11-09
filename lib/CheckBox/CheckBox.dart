119cc82bb2f0c5f61668c8219fb4e46e81fed2d8
IsmailParacha <ismailjanparacha@gmail.com>
2022-11-09 23:21:14 +0500

    CheckBox

---
+++ lib/CheckBox/CheckBox.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CheckBoxEample extends StatefulWidget {
+  CheckBoxEample({Key? key}) : super(key: key);
+
+  @override
+  State<CheckBoxEample> createState() => _CheckBoxEampleState();
+}
+
+class _CheckBoxEampleState extends State<CheckBoxEample> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Checkbox"),
+      ),
+      body: Center(
+        child: Theme(
+          data: Theme.of(context).copyWith(unselectedWidgetColor: Colors.red),
+          child: CheckboxListTile(
+            title: Text("Check requirements"),
+            subtitle: Text("Checking"),
+            value: isChecked,
+            onChanged: (value) {
+              setState(
+                () {
+                  isChecked = value!;
+                },
+              );
+            },
+            activeColor: Colors.red,
+            checkColor: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ms_global/CheckBox/CheckBox.dart';
 import 'package:ms_global/ExpansionTile/Expansiontile.dart';
 import 'package:ms_global/Get%20State/Routes/mainPage.dart';
 import 'package:ms_global/Grid%20view/grid.dart';
@@ -29,7 +30,7 @@ class _MyAppState extends State<MyApp> {
       scaffoldMessengerKey: messengerKey,
       navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
-      home: ExpansionTileExample(),
+      home: CheckBoxEample(),
       // home: Scaffold(
       //   // body: Property(),
       //   body: Center(
